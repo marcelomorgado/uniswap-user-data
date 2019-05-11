f227ed2b7c36d902057351f36e32c6bea1c301aa
cmarcelo <cmarcelom@gmail.com>
2019-05-11 16:10:42 +0100

    Apollo installation

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import fetch from "node-fetch";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { gql } from "apollo-boost";
+
+import ApolloClient from "apollo-client";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it.skip("fetching data", async () => {
+  jest.setTimeout(10000);
+  const client = new ApolloClient({
+    link: createHttpLink({
+      uri: "https://48p1r2roz4.sse.codesandbox.io",
+      fetch: fetch,
+    }),
+    cache: new InMemoryCache(),
+  });
+
+  const result = await client.query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `,
+  });
+  console.log(result);
 });
