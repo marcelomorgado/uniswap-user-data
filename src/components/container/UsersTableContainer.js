40349a6437e710f4b0066cfab25136c91f4a1cd3
cmarcelo <cmarcelom@gmail.com>
2019-05-12 20:03:53 +0100

    Infinity users

--- src/components/container/UsersTableContainer.js
+++ src/components/container/UsersTableContainer.js
@@ -1,36 +1,99 @@
 import React from "react";
-import UsersTable from "../presentational/UsersTable";
+//import UsersTable from "../presentational/UsersTable";
+import UsersInfinityList from "../presentational/UsersInfinityList";
 import PropTypes from "prop-types";
 import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
 
-const usersToRows = users => {
-  const rows = users.map(user => {
-    return { ...user, etherBalance: "-" };
-  });
-
-  return rows;
-};
+const ITEMS_PER_PAGE = 5;
 
 const GET_USERS = gql`
-  {
-    users {
+  query User($itemsPerPage: Int) {
+    users(first: $itemsPerPage) {
       id
     }
   }
 `;
 
+const GET_MORE_USERS = gql`
+  query User($cursor: String, $itemsPerPage: Int) {
+    users(first: $itemsPerPage, where: { id_gt: $cursor }) {
+      id
+    }
+  }
+`;
+
+// const usersToRows = users => {
+//   const rows = users.map(user => {
+//     return { ...user, etherBalance: "-" };
+//   });
+//
+//   return rows;
+// };
+
 class UsersTableContainer extends React.Component {
+  state = {
+    hasNextPage: true,
+    isNextPageLoading: false,
+  };
+
   render() {
+    const { hasNextPage, isNextPageLoading } = this.state;
+    const { onRowClick } = this.props;
     return (
-      <Query query={GET_USERS}>
-        {({ loading, error, data }) => {
+      <Query query={GET_USERS} variables={{ itemsPerPage: ITEMS_PER_PAGE }}>
+        {({ loading, error, data, fetchMore }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Error :(</p>;
           const { users } = data;
-          const rows = usersToRows(users);
 
-          return <UsersTable rows={rows} onRowClick={this.props.onRowClick} />;
+          if (!users.length) {
+            this.setState({ hasNextPage: false });
+          }
+
+          const onLoadMore = () => {
+            const lastUser = users[users.length - 1];
+            const { id: cursor } = lastUser;
+
+            this.setState({ isNextPageLoading: true });
+
+            const updateQuery = (prevResult, { fetchMoreResult: newData }) => {
+              const { users: prevUsers } = prevResult;
+              const { users: newUsers } = newData;
+              const hasNewData = newUsers.length;
+              let data;
+
+              if (!hasNewData) {
+                data = prevResult;
+                this.setState({ hasNextPage: false });
+              } else {
+                const users = [...prevUsers, ...newUsers];
+                data = { users };
+              }
+
+              this.setState({ isNextPageLoading: false });
+              return data;
+            };
+
+            return fetchMore({
+              query: GET_MORE_USERS,
+              variables: {
+                cursor,
+                itemsPerPage: ITEMS_PER_PAGE,
+              },
+              updateQuery,
+            });
+          };
+          return (
+            <UsersInfinityList
+              items={users}
+              loadNextPage={onLoadMore}
+              hasNextPage={hasNextPage}
+              isNextPageLoading={isNextPageLoading}
+              onTransactionsClick={onRowClick}
+            />
+          );
+          //  return <UsersTable rows={users} onRowClick={this.props.onRowClick} />;
         }}
       </Query>
     );
---
+++ src/components/presentational/User.js
@@ -0,0 +1,82 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const styles = theme => ({
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
+
+function User({ user, style, classes, handleTransactions }) {
+  const { id, etherBalance } = user;
+
+  return (
+    <Card className={classes.card} style={style}>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          {id}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          {`${etherBalance} ETH`}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          onClick={() => {
+            handleTransactions(id);
+          }}
+        >
+          Transactions
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          className={classes.button}
+          onClick={() => {}}
+        >
+          Transfer from
+        </Button>
+      </CardActions>
+    </Card>
+  );
+  // return (
+  //   <div style={style}>
+  //     <div>
+  //       {id} - {etherBalance}
+  //     </div>
+  //   </div>
+  // );
+}
+
+User.propTypes = {
+  user: PropTypes.object.isRequired,
+  style: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+  handleTransactions: PropTypes.func.isRequired,
+};
+
+//export default User;
+export default withStyles(styles)(User);
