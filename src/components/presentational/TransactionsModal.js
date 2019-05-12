2323b81ce690fe3aac743e323bc11a73f2dee44d
cmarcelo <cmarcelom@gmail.com>
2019-05-12 16:55:26 +0100

    Refactoring

--- src/components/presentational/TransactionsModal.js
+++ src/components/presentational/TransactionsModal.js
@@ -3,13 +3,7 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import Button from "@material-ui/core/Button";
-import { FixedSizeList as List } from "react-window";
-import InfiniteLoader from "react-window-infinite-loader";
-
-import styles from "./InfiniteLoader.example.css";
-
-const STATUS_LOADING = 1;
-const STATUS_LOADED = 2;
+import TransactionsInfiniteList from "./TransactionsInfiniteList";
 
 function getModalStyle() {
   const top = 50;
@@ -57,7 +51,7 @@ class TransactionsModal extends React.Component {
           <div style={getModalStyle()} className={classes.paper}>
             <Button onClick={onLoadMore}>LoadMore</Button>
 
-            <ExampleWrapper
+            <TransactionsInfiniteList
               items={transactions}
               loadNextPage={onLoadMore}
               hasNextPage={hasNextPage}
@@ -82,75 +76,3 @@ TransactionsModal.propTypes = {
 };
 
 export default withStyles(modalStyles)(TransactionsModal);
-
-ExampleWrapper.propTypes = {
-  hasNextPage: PropTypes.bool.isRequired,
-  isNextPageLoading: PropTypes.bool.isRequired,
-  loadNextPage: PropTypes.func.isRequired,
-  items: PropTypes.array.isRequired,
-};
-
-function ExampleWrapper({
-  // Are there more items to load?
-  // (This information comes from the most recent API request.)
-  hasNextPage,
-
-  // Are we currently loading a page of items?
-  // (This may be an in-flight flag in your Redux store for example.)
-  isNextPageLoading,
-
-  // Array of items loaded so far.
-  items,
-
-  // Callback function responsible for loading the next page of items.
-  loadNextPage,
-}) {
-  // If there are more items to be loaded then add an extra row to hold a loading indicator.
-  const itemCount = hasNextPage ? items.length + 1 : items.length;
-
-  // Only load 1 page of items at a time.
-  // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
-  const loadMoreItems = isNextPageLoading ? () => {} : loadNextPage;
-
-  // Every row is loaded except for our loading indicator row.
-  const isItemLoaded = index => !hasNextPage || index < items.length;
-
-  // Render an item or a loading indicator.
-  const Item = ({ index, style }) => {
-    let content;
-    if (!isItemLoaded(index)) {
-      content = "Loading...";
-    } else {
-      content = items[index].id;
-    }
-
-    return <div style={style}>{content}</div>;
-  };
-
-  Item.propTypes = {
-    index: PropTypes.number.isRequired,
-    style: PropTypes.object.isRequired,
-  };
-
-  return (
-    <InfiniteLoader
-      isItemLoaded={isItemLoaded}
-      itemCount={itemCount}
-      loadMoreItems={loadMoreItems}
-    >
-      {({ onItemsRendered, ref }) => (
-        <List
-          className="List"
-          height={150}
-          itemCount={itemCount}
-          itemSize={30}
-          onItemsRendered={onItemsRendered}
-          ref={ref}
-          width={300}
-        >
-          {Item}
-        </List>
-      )}
-    </InfiniteLoader>
-  );
-}
